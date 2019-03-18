f44d8ace16fa9e3c013b260964eeeafc00a64a04
46treasure <45ytrasure@gmail.com>
2019-03-18 15:59:37 +0200

    new tags

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,26 @@ import { NgModule } from '@angular/core';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
+import { HomeComponent } from './home/home.component';
+import {RouterModule, Routes} from '@angular/router';
+import { UserpageComponent } from './userpage/userpage.component';
 
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'userpage', component: UserpageComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    UserpageComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    NgbModule.forRoot()
+    NgbModule.forRoot(),
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
