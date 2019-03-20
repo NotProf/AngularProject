c808407c5e51b820b8ad238cf543aba02c3655f1
46treasure <45ytrasure@gmail.com>
2019-03-20 14:48:00 +0200

    new changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  title = 'slider';
+  el = document.getElementsByClassName('slid')
 
-  ngOnInit() {
+  count = 0;
+  images = ['assets\\slide1.jpg', 'assets\\slide2.jpg', 'assets\\slide3.jpg', 'assets\\slide4.jpg', 'assets\\slide5.jpg']
+  image = this.images[this.count];
+
+  next() {
+    this.count++;
+    if (this.count >= this.images.length) {
+      this.count = 0;
+    }
+    this.image = this.images[this.count];
   }
 
+  back() {
+    this.count--;
+    if (this.count <= 0) {
+      this.count = this.images.length - 1;
+    }
+    this.image = this.images[this.count];
+  }
+
+  ngOnInit(): void {
+    setInterval(() => this.next(), 5000);
+
+  }
 }
