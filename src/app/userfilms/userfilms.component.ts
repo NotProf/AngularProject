7afaa986e107dca19bc17e4e6eec5fa96f78b72d
46treasure <45ytrasure@gmail.com>
2019-05-18 19:15:08 +0300

    userpage

--- src/app/userfilms/userfilms.component.ts
+++ src/app/userfilms/userfilms.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-userfilms',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./userfilms.component.css']
 })
 export class UserfilmsComponent implements OnInit {
-
+  @Input() public allFilms: string;
   constructor() { }
 
   ngOnInit() {
