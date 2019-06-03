099ac247e0d7a712ed428e22b759e0ff1d10f234
46treasure <45ytrasure@gmail.com>
2019-06-03 16:13:20 +0300

    Merge remote-tracking branch 'origin/MainTest' into MainTest
    # Conflicts:
    #	src/app/home/home.component.html
    #	src/app/home/home.component.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,7 +23,8 @@ import {UserfilmsComponent} from './userfilms/userfilms.component';
 import {NoAuthUserDirective} from './no-auth-user.directive';
 import {UserAuthDirective} from './user-auth.directive';
 import {UsersfriendsComponent} from './usersfriends/usersfriends.component';
-import { AddButtonDirective } from './add-button.directive';
+import {AddButtonDirective} from './add-button.directive';
+import { SearchComponent } from './search/search.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
@@ -47,7 +48,8 @@ const routes: Routes = [
   {path: 'contacts', component: ContactsComponent},
   {path: 'addfilm', component: AddfilmComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'reg', component: RegComponent}
+  {path: 'reg', component: RegComponent},
+  {path: 'search', component: SearchComponent}
 ];
 
 @NgModule({
@@ -71,7 +73,8 @@ const routes: Routes = [
     NoAuthUserDirective,
     UserAuthDirective,
     UsersfriendsComponent,
-    AddButtonDirective
+    AddButtonDirective,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
